f65b06715463a9ac4cfc966b7017796af0bfce0e
Nikita Singh <nikita_singh@external.mckinsey.com>
2015-04-14 18:48:49 +0530

    adding rails api application as server

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Server::Application.config.secret_token = '5512035139337f78b04e505e51a018cb6b38d20b60196b98c3748ee104aa670dd2a74b165ad123ab67058984f647359f8466563bde51cf9432fd63191731746b'
